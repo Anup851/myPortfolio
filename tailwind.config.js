09fcf55edda419be5b0f83e2663cf51f3cd9c360
Anup singh <162133748+Anup851@users.noreply.github.com>
2025-08-29 19:56:35 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
-  theme: {
-    extend: {},
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 }
+
+
